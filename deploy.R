5f27c87883de49897434111e17c8a9d247370fb0
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2025-04-30 14:32:16 +0100

    Add double-deploy script

---
+++ deploy.R
@@ -0,0 +1,14 @@
+deploy <- function(server_name, app_id) {
+  rsconnect::deployDoc(
+    server = server_name,
+    appId = app_id,
+    doc = "index.qmd",
+    appName = "posit-connect-people",
+    appTitle = "Lookup: Posit Connect People",
+    lint = FALSE,
+    forceUpdate = TRUE
+  )
+}
+
+deploy("connect.strategyunitwm.nhs.uk", 321)
+deploy("connect.su.mlcsu.org", 82)
